505bf78205584ccb0e919e2959933172bf605104
gloriafercu <gloriafercu@gmail.com>
2018-05-31 17:58:38 +0200

    add promises and fetch

--- src/main.js
+++ src/main.js
@@ -1,29 +1,35 @@
-// function getPersonalData(url) {
+/* RESUELTO CON PROMESAS */
+
+// function getPersonalData() {
 // 	return new Promise(function(resolve, reject) {
 // 		let request = new XMLHttpRequest();
 // 		request.addEventListener('load', function() {
 // 			let data = JSON.parse(request.responseText);
 // 			resolve(data);
+// 			console.log('data: ', data);
+// 			for (var i = 0; i < data.length; i++) {
+// 				console.log(`Me llamo ${data[i].name} ${data[i].surname} y tengo ${data[i].age} años`);
+// 			}
 // 		});
 // 		request.open('GET', 'http://localhost:3000/api/personal-data');
 // 		request.send();
 // 	});
-// 	return promiseData;
 // }
+// getPersonalData();
+
+
+/* RESUELTO CON FETCH */
 
 
 fetch('http://localhost:3000/api/personal-data')
 	.then(function(response) {
-		//console.log('objeto parseado: ', response.json());
 		return response.json();
 	})
 	.then(function(json) {
-
 		let data = json;
 		console.log('data: ', data);
 		for (var i = 0; i < data.length; i++) {
 			data[i].name;
 			console.log(`Me llamo ${data[i].name} ${data[i].surname} y tengo ${data[i].age} años`);
 		}
-
 	});
