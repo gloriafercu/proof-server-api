ea62f5150a96fefc628db45a85861285b57149ea
gloriafercu <gloriafercu@gmail.com>
2018-05-31 16:07:13 +0200

    change to promises

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
-function getPersonalData() {
-	let request = new XMLHttpRequest();
+function getPersonalData(url) {
+	return new Promise(function(resolve, reject) {
+	const request = new XMLHttpRequest();
 	request.addEventListener('load', getPersonalData);
-	let data = JSON.parse(request.responseText);
 	request.open('GET', 'http://localhost:3000/api/personal-data');
+	let data = JSON.parse(request.responseText);
 	request.send();
+	});
+	// Manejar cuando la promesa se haya cumplido
+	promiseData.then(function(data) {
+		console.log('Promesa cumplida: ', data);
+	});
+	// Manejar cuando la promesa se haya rechazado
+	promiseData.catch(function(error) {
+		console.log('Promesa rechazada: ', error);
+	});
 }
+
+
+// fetch('http://localhost:3000/api/personal-data')
+// 	.then(function(response) {
+// 		console.log('objeto parseado: ', response.json());
+// 		return response.json();
+// 	})
+// 	.then(function(json) {
+// 		var data = json;
+// 		console.log(data);
+// 	});
