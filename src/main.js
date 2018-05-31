63c92741033cf0f88469ab79efbf47b44ac74d09
gloriafercu <gloriafercu@gmail.com>
2018-05-31 17:13:29 +0200

    add fetch

--- src/main.js
+++ src/main.js
@@ -1,28 +1,29 @@
-function getPersonalData(url) {
-	return new Promise(function(resolve, reject) {
-	const request = new XMLHttpRequest();
-	request.addEventListener('load', getPersonalData);
-	request.open('GET', 'http://localhost:3000/api/personal-data');
-	let data = JSON.parse(request.responseText);
-	request.send();
-	});
-	// Manejar cuando la promesa se haya cumplido
-	promiseData.then(function(data) {
-		console.log('Promesa cumplida: ', data);
-	});
-	// Manejar cuando la promesa se haya rechazado
-	promiseData.catch(function(error) {
-		console.log('Promesa rechazada: ', error);
-	});
-}
-
-
-// fetch('http://localhost:3000/api/personal-data')
-// 	.then(function(response) {
-// 		console.log('objeto parseado: ', response.json());
-// 		return response.json();
-// 	})
-// 	.then(function(json) {
-// 		var data = json;
-// 		console.log(data);
+// function getPersonalData(url) {
+// 	return new Promise(function(resolve, reject) {
+// 		let request = new XMLHttpRequest();
+// 		request.addEventListener('load', function() {
+// 			let data = JSON.parse(request.responseText);
+// 			resolve(data);
+// 		});
+// 		request.open('GET', 'http://localhost:3000/api/personal-data');
+// 		request.send();
 // 	});
+// 	return promiseData;
+// }
+
+
+fetch('http://localhost:3000/api/personal-data')
+	.then(function(response) {
+		//console.log('objeto parseado: ', response.json());
+		return response.json();
+	})
+	.then(function(json) {
+
+		let data = json;
+		console.log('data: ', data);
+		for (var i = 0; i < data.length; i++) {
+			data[i].name;
+			console.log(`Me llamo ${data[i].name} ${data[i].surname} y tengo ${data[i].age} años`);
+		}
+
+	});
